9fc6463dbe1b783b64a345dc4046e6ca8c40346f
AlexandreBesia <alexandre.besia@he-arc.ch>
2022-09-28 16:56:25 +0200

    add a bit of bootstrap to the about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,4 +1,17 @@
 @extends("layout.app")
 @section("content")
-hello about
+
+<div class="container-sm">
+    <div class="row justify-content-center">
+        <div class="col-sm-12 col-md-6">
+            <div class="row mt-3 mb-3">
+                <h3>About</h3>
+            </div>
+            <div class="row g-3">
+                <p>We are Vapor, we've been selling the latest and greatest games since 1995.
+            </div>
+        </div>
+    </div>
+</div>
+
 @endsection
