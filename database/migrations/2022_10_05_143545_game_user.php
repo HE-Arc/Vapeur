69114c6dfc9c7452127687a280352f4a64851c96
Cyrille Polier <cyrille.polier@he-arc.ch>
2022-10-05 16:43:40 +0200

    #1 Add migration for generating all tables in DB

---
+++ database/migrations/2022_10_05_143545_game_user.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('game_user', function (Blueprint $table) {
+            $table->id();
+            $table
+                ->foreignId("game_id")
+                ->constrained()
+                ->onUpdate("cascade")
+                ->onDelete("no action");
+            $table
+                ->foreignId("user_id")
+                ->constrained()
+                ->onUpdate("cascade")
+                ->onDelete("no action");
+            $table->date("date");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
