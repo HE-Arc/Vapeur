d99871800b71333cc7289d0ddce979812e319474
Filippos Gleglakos <filippos.gleglakos@he-arc.ch>
2022-10-05 19:49:46 +0200

    #8 Added temporary user for testing purposes.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
     {
         \App\Models\User::truncate();
         \App\Models\User::factory()->create([
-            'name' => 'Test User',
+            'name' => 'test',
             'email' => 'test@example.com',
             'password' => password_hash('test', PASSWORD_DEFAULT)
         ]);
