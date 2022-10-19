46c2e993631818ad710c9b0f6cff3046e6762fed
Filippos Gleglakos <filippos.gleglakos@he-arc.ch>
2022-10-19 16:43:58 +0200

    #17 Removed the TODO comments.

--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -5,14 +5,12 @@
     @foreach ($games as $game)
         <div class="col">
             <div class="card text-dark bg-light">
-                <!-- TODO: Add link to game page -->
                 <a href="{{route("games.show", $game->id)}}">
                     <!-- TODO: Find better way to retrieve images as it's probably not the best way -->
                     <img class="card-img-top" src="{{asset("images/{$game->image_path}")}}" alt="{{$game->name}}">
                 </a>
                 <div class="card-body">
                     <h5 class="card-title text-truncate" data-bs-toggle="tooltip" data-bs-placement="top" title="{{$game->name}}">
-                        <!-- TODO: Add link to game page -->
                         <a href="{{route("games.show", $game->id)}}">{{$game->name}}</a>
                     </h5>
                     <h6 class="card-subtitle mb-2 text-muted">${{$game->price}}</h6>
