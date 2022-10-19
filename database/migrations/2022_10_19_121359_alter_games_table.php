a8b958ee73e920163681389ad6915a8ee3a543af
Filippos Gleglakos <filippos.gleglakos@he-arc.ch>
2022-10-19 14:26:05 +0200

    #19 Fixed the character limits for the 'games' table in the DB.

---
+++ database/migrations/2022_10_19_121359_alter_games_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('games', function (Blueprint $table) {
+            $table->string('description', 4096)->change();
+            $table->string('image_path', 1024)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('games', function (Blueprint $table) {
+            $table->string('description')->change();
+            $table->string('image_path')->change();
+        });
+    }
+};
