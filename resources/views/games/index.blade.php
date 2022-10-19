1fb6234fdcd1d714161e388bafcded63866ee296
Filippos Gleglakos <filippos.gleglakos@he-arc.ch>
2022-10-19 16:31:02 +0200

    #17 Added the correct links for the games pages in index.

--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -6,14 +6,14 @@
         <div class="col">
             <div class="card text-dark bg-light">
                 <!-- TODO: Add link to game page -->
-                <a href="#">
+                <a href="{{route("games.show", $game->id)}}">
                     <!-- TODO: Find better way to retrieve images as it's probably not the best way -->
                     <img class="card-img-top" src="{{asset("images/{$game->image_path}")}}" alt="{{$game->name}}">
                 </a>
                 <div class="card-body">
                     <h5 class="card-title text-truncate" data-bs-toggle="tooltip" data-bs-placement="top" title="{{$game->name}}">
                         <!-- TODO: Add link to game page -->
-                        <a href="#">{{$game->name}}</a>
+                        <a href="{{route("games.show", $game->id)}}">{{$game->name}}</a>
                     </h5>
                     <h6 class="card-subtitle mb-2 text-muted">${{$game->price}}</h6>
                     <p class="card-text">{{$game->description}}</p>
