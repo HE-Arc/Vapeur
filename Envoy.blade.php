57e6abac3f6e0f140e048e41c4aab11890efd0ee
Cyrille Polier <cyrille.polier@he-arc.ch>
2022-10-21 22:06:48 +0200

    #23 Add CD using laravel/Envoy

---
+++ Envoy.blade.php
@@ -0,0 +1,34 @@
+@servers(['web' => $_ENV['SERVER']])
+
+@setup
+    $path = $_ENV['PATH' ];
+@endsetup
+
+@story('deploy')
+    git
+    composer
+    artisan
+    optimize
+@endstory
+
+@task('git', ['on' => 'web'])
+    cd {{ $path }}
+    git pull origin master
+@endtask
+
+@task('composer', ['on' => 'web'])
+    cd {{ $path }}
+    composer install --no-interaction --no-dev --prefer-dist --optimize-autoloader
+@endtask
+
+@task('artisan', ['on' => 'web'])
+    cd {{ $path }}
+    php artisan migrate --force
+@endtask
+
+@task('optimize', ['on' => 'web'])
+    cd {{ $path }}
+    php artisan config:cache
+    php artisan route:cache {{-- not yet necessary as the number of route is small --}}
+    php artisan view:cache
+@endtask
