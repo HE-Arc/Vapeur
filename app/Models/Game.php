7ff9d08202a6b3c7f46f646cda0f1901afbdff73
Cyrille Polier <cyrille.polier@he-arc.ch>
2022-10-29 16:01:42 +0200

    #10 add N to N relation for game_genre

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Game extends Model
 {
     use HasFactory;
+
+
+    public function genres()
+    {
+        return $this->belongsToMany(Genre::class);
+    }
 }
