bc7ac07a924030af46fa969f5090545d8941a92e
Besia Alexandre <alexandre.besia@he-arc.ch>
2022-11-30 14:37:32 +0100

    fix view index in games (the one that show all the games)
    all card now have the same height

--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -1,33 +1,35 @@
 @extends("layout.app")
 
 @section("content")
+
 @if (!empty(session('userId')))
-<a class="btn btn-dark" href="{{ route('games.create') }}">Add game</a>
+	<a class="btn btn-dark" href="{{ route('games.create') }}">Add game</a>
 @endif
 
-
-<div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 mt-2 g-4">
-    @foreach ($games as $game)
-        <div class="col">
-            <div class="card text-dark bg-light">
-                <a href="{{route("games.show", $game->id)}}">
-                    <!-- TODO: Find better way to retrieve images as it's probably not the best way -->
-                    <img class="card-img-top" src="{{asset("images/{$game->image_path}")}}" alt="{{$game->name}}">
-                </a>
-                <div class="card-body">
-                    <h5 class="card-title text-truncate" data-bs-toggle="tooltip" data-bs-placement="top" title="{{$game->name}}">
-                        <a href="{{route("games.show", $game->id)}}">{{$game->name}}</a>
-                    </h5>
-                    <h6 class="card-subtitle mb-2 text-muted">${{$game->price}}</h6>
-                    <p class="card-text">{{$game->description}}</p>
-                </div>
-                <div class="card-footer">
-                    @foreach ($game->genres as $genre)
-                        <span class="badge bg-secondary">{{$genre->name}}</span>
-                    @endforeach
-                </div>
-            </div>
-        </div>
-    @endforeach
+<div class="row row-cols-1 row-cols-md-3 g-4">
+	@foreach ($games as $game)
+	<div class="col">
+		<div class="card h-100">
+            <a href="{{route("games.show", $game->id)}}">
+			<img class="card-img-top" src="{{asset("images/{$game->image_path}")}}" alt="{{$game->name}}">
+				<div class="card-body">
+					<h5 class="card-title text-truncate" data-bs-toggle=
+						<a href="{{route("games.show", $game->id)}}">{{$game->name}}</a>
+					</h5>
+					<h6 class="card-subtitle mb-2 text-muted">${{$game->price}}</h6>
+					<p class="card-text">{{$game->description}}</p>
+				</div>
+				<div class="card-footer">
+					<small class="text-muted">
+						@foreach ($game->genres as $genre)
+							<span class="badge bg-secondary">{{$genre->name}}</span>
+						@endforeach
+					</small>
+				</div>
+			</div>
+		</div>
+	@endforeach
+    {!! $games->links() !!}
 </div>
+
 @endsection
