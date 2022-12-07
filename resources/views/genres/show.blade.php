7bc8dcd6a2fc541cd124403cf91634bd95503d0e
Cyrille Polier <cyrille.polier@he-arc.ch>
2022-12-07 13:35:52 +0100

    Add genres #43

---
+++ resources/views/genres/show.blade.php
@@ -0,0 +1,35 @@
+@extends('layout.app')
+
+
+@section('content')
+    <div class="row">
+        <div class="col-12">
+            <h1>{{$genre->name}}</h1>
+        </div>
+    </div>
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+        @foreach ($genre->games as $game)
+        <div class="col">
+            <div class="card h-100">
+                <a href="{{route("games.show", $game->id)}}">
+                <img class="card-img-top" src="{{asset("images/{$game->image_path}")}}" alt="{{$game->name}}">
+                    <div class="card-body">
+                        <h5 class="card-title text-truncate" data-bs-toggle=
+                            <a href="{{route("games.show", $game->id)}}">{{$game->name}}</a>
+                        </h5>
+                        <h6 class="card-subtitle mb-2 text-muted">${{$game->price}}</h6>
+                        <p class="card-text">{{$game->description}}</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">
+                            @foreach ($game->genres as $genre)
+                                <span class="badge bg-secondary">{{$genre->name}}</span>
+                            @endforeach
+                        </small>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+
+@endsection
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -26,6 +26,8 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
+                        <a class="nav-link" href="{{ route('genres.index') }}">Genres</a>
+                    <li class="nav-item">
                         <a class="nav-link" href="{{ route('about') }}">About</a>
                     </li>
                 </ul>
