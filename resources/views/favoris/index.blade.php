2c93fae5c33323c81b552b7380d645307e8b2e81
Filippos Gleglakos <filippos.gleglakos@he-arc.ch>
2022-12-14 13:47:09 +0100

    The favourites page now also shows the game banner, a link to the game's page, and links to the genres.

--- resources/views/favoris/index.blade.php
+++ resources/views/favoris/index.blade.php
@@ -1,14 +1,15 @@
 @extends("layout.app")
 @section("content")
 
-<h1>Jeu en favoris</h1>
+<h1>Your favourites</h1>
 
 <div class="table-responsive">
     <table class="table align-middle">
         <thead>
             <tr>
-                <th scope="col">Genre</th>
-                <th scope="col">Nom</th>
+                <th scope="col"></th>
+                <th scope="col">Title</th>
+                <th scope="col">Genre(s)</th>
                 <th scope="col">&nbsp;</th>
             </tr>
         </thead>
@@ -16,17 +17,25 @@
             @foreach ($favoris as $game)
             <tr>
                 <td>
+                    <img class="img-fluid w-50" src="{{asset("images/{$game->image_path}")}}" alt="{{$game->name}}">
+                </td>
+                <td>
+                    <h5>
+						<a href="{{route("games.show", $game->id)}}">{{$game->name}}</a>
+					</h5>
+                </td>
+                <td>
                     @foreach ($game->genres as $genre)
-                        <span>{{$genre->name}}</span>
+                        <a href="{{ route("genres.show", $genre->id)}}"><span class="badge bg-secondary">{{$genre->name}}</span></a>
                     @endforeach
                 </td>
-                <td>{{$game->name}}</td>
                 <td>
-                <form action="{{ route('favoris.destroy', $game->id) }}" method="POST">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit" class="btn btn-danger">Supprimer des favoris</button>
-                </form>
+                    <form action="{{ route('favoris.destroy', $game->id) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger">Remove</button>
+                    </form>
+                </td>
             </tr>
         @endforeach
         </tbody>
